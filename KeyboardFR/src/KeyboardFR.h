f68e7250843555d62a1512623b310cf92bb56b6b
mtsimon <matthieu.simon.2@inrae.fr>
2021-05-05 08:10:40 +0200

    fix modifiers problem

--- KeyboardFR/src/KeyboardFR.h
+++ KeyboardFR/src/KeyboardFR.h
@@ -34,43 +34,43 @@
 //================================================================================
 //  Keyboard
 
-#define KEY_LEFT_CTRL		0x80
-#define KEY_LEFT_SHIFT		0x81
-#define KEY_LEFT_ALT		0x91
+#define KEY_LEFT_CTRL		0x01
+#define KEY_LEFT_SHIFT		0x02
+#define KEY_LEFT_ALT		0x04
 //#define KEY_LEFT_GUI		0x83
-#define KEY_LEFT_GUI		0x83
-#define KEY_RIGHT_CTRL		0x84
-#define KEY_RIGHT_SHIFT		0x85
-#define KEY_RIGHT_ALT		0x86
-#define KEY_RIGHT_GUI		0x87
+#define KEY_LEFT_GUI		0x08
+#define KEY_RIGHT_CTRL		0x10
+#define KEY_RIGHT_SHIFT		0x20
+#define KEY_RIGHT_ALT		0x40
+#define KEY_RIGHT_GUI		0x80
 
-#define KEY_UP_ARROW		0xDA
-#define KEY_DOWN_ARROW		0xD9
-#define KEY_LEFT_ARROW		0xD8
-#define KEY_RIGHT_ARROW		0xD7
-#define KEY_BACKSPACE		0xB2
-#define KEY_TAB				0xB3
-#define KEY_RETURN			0xB0
-#define KEY_ESC				0xB1
-#define KEY_INSERT			0xD1
-#define KEY_DELETE			0xD4
-#define KEY_PAGE_UP			0xD3
-#define KEY_PAGE_DOWN		0xD6
-#define KEY_HOME			0xD2
-#define KEY_END				0xD5
-#define KEY_CAPS_LOCK		0xC1
-#define KEY_F1				0xC2
-#define KEY_F2				0xC3
-#define KEY_F3				0xC4
-#define KEY_F4				0xC5
-#define KEY_F5				0xC6
-#define KEY_F6				0xC7
-#define KEY_F7				0xC8
-#define KEY_F8				0xC9
-#define KEY_F9				0xCA
-#define KEY_F10				0xCB
-#define KEY_F11				0xCC
-#define KEY_F12				0xCD
+#define KEY_UP_ARROW		0x52
+#define KEY_DOWN_ARROW		0x51
+#define KEY_LEFT_ARROW		0x50
+#define KEY_RIGHT_ARROW		0x49
+#define KEY_BACKSPACE		0x2A
+#define KEY_TAB				0x2B
+#define KEY_RETURN			0x28
+#define KEY_ESC				0x29
+#define KEY_INSERT			0x49
+#define KEY_DELETE			0x4C
+#define KEY_PAGE_UP			0x4B
+#define KEY_PAGE_DOWN		0x4E
+#define KEY_HOME			0x4A
+#define KEY_END				0x4D
+#define KEY_CAPS_LOCK		0x39
+#define KEY_F1				0x3A
+#define KEY_F2				0x3B
+#define KEY_F3				0x3C
+#define KEY_F4				0x3D
+#define KEY_F5				0x3E
+#define KEY_F6				0x3F
+#define KEY_F7				0x40
+#define KEY_F8				0x41
+#define KEY_F9				0x42
+#define KEY_F10				0x43
+#define KEY_F11				0x44
+#define KEY_F12				0x45
 
 
 //  Low level key report: up to 6 keys and shift, ctrl etc at once
